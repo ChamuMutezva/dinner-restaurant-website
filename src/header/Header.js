3df045b29aaecf6258fe0f61a27ba1bc3a348845
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-03-05 19:50:06 +0200

    feat: initial and starter oack
    dine restaurant website app launch

---
+++ src/header/Header.js
@@ -0,0 +1,36 @@
+import heroMobile from '../assets/images/homepage/hero-bg-mobile.jpg'
+import heroMobile2 from '../assets/images/homepage/hero-bg-mobile@2x.jpg'
+import heroTablet from '../assets/images/homepage/hero-bg-tablet.jpg'
+import heroTablet2 from '../assets/images/homepage/hero-bg-tablet@2x.jpg'
+import heroDesktop from '../assets/images/homepage/hero-bg-desktop.jpg'
+import heroDesktop2 from '../assets/images/homepage/hero-bg-desktop@2x.jpg'
+import logo from '../assets/images/logo.svg'
+import '../styles/style.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="hero">
+                <picture>
+                    <source media="(max-width: 680px)" srcSet={`${heroMobile}, ${heroMobile2}`} />
+                    <source media="(max-width: 1080px)" srcSet={`${heroTablet}, ${heroTablet2}`} />
+                    <source media="(min-width: 1081px)" srcSet={`${heroDesktop}, ${heroDesktop2}`} />
+                    <img src={heroMobile} srcSet={`${heroMobile}, ${heroTablet}, ${heroDesktop}`}
+                     alt="Enjoy your meal today"/>
+                </picture>
+                <div className="hero__main">
+                    <img src={logo} alt="welcome to dine" />
+
+                    <h1> Exquisite dining since 1989</h1>
+                    <p>
+                        Experience our seasonal menu in beautiful country surroundings.
+                        Eat the freshest produce from
+                        the comfort of our farmhouse.
+                    </p>
+                    <button>Book a table</button>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header
