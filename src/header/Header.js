79f34765f9b1c9c13888858de885aac31362eab1
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-03-06 19:06:39 +0200

    feat: tablet - header setup

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,16 +1,18 @@
-import heroMobile from '../assets/images/homepage/hero-bg-mobile.jpg'
-import heroMobile2 from '../assets/images/homepage/hero-bg-mobile@2x.jpg'
-import heroTablet from '../assets/images/homepage/hero-bg-tablet.jpg'
-import heroTablet2 from '../assets/images/homepage/hero-bg-tablet@2x.jpg'
-import heroDesktop from '../assets/images/homepage/hero-bg-desktop.jpg'
-import heroDesktop2 from '../assets/images/homepage/hero-bg-desktop@2x.jpg'
+//import heroMobile from '../assets/images/homepage/hero-bg-mobile.jpg'
+//import heroMobile2 from '../assets/images/homepage/hero-bg-mobile@2x.jpg'
+//import heroTablet from '../assets/images/homepage/hero-bg-tablet.jpg'
+//import heroTablet2 from '../assets/images/homepage/hero-bg-tablet@2x.jpg'
+//import heroDesktop from '../assets/images/homepage/hero-bg-desktop.jpg'
+//import heroDesktop2 from '../assets/images/homepage/hero-bg-desktop@2x.jpg'
 import logo from '../assets/images/logo.svg'
+import Button from '../shared-components/Button'
 import '../styles/style.css'
 
 const Header = () => {
     return (
         <header>
             <div className="hero">
+                {/*
                 <picture>
                     <source media="(max-width: 680px)" srcSet={`${heroMobile}, ${heroMobile2}`} />
                     <source media="(max-width: 1080px)" srcSet={`${heroTablet}, ${heroTablet2}`} />
@@ -18,16 +20,20 @@ const Header = () => {
                     <img src={heroMobile} srcSet={`${heroMobile}, ${heroTablet}, ${heroDesktop}`}
                      alt="Enjoy your meal today"/>
                 </picture>
+                */}
                 <div className="hero__main">
-                    <img src={logo} alt="welcome to dine" />
+                    <img className="hero__main__logo" src={logo} alt="welcome to dine" />
 
-                    <h1> Exquisite dining since 1989</h1>
-                    <p>
+                    <h1 className="hero__main__title"> Exquisite dining since 1989</h1>
+                    <p className="hero__main__descr">
                         Experience our seasonal menu in beautiful country surroundings.
                         Eat the freshest produce from
                         the comfort of our farmhouse.
                     </p>
-                    <button>Book a table</button>
+                    div
+                    
+                    <Button title="Book a table" />
+
                 </div>
             </div>
         </header>
---
+++ src/shared-components/Button.js
@@ -0,0 +1,6 @@
+const Button = (props) => {
+    return(
+        <button>{props.title}</button>
+    )
+}
+export default Button
