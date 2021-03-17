9c23c99766daa9de9f8d3f032418404a8b6135b6
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-03-17 06:18:54 +0200

    desktop menu section flex in use

--- src/main/cardComponent.js
+++ src/main/cardComponent.js
@@ -5,8 +5,9 @@ const CardComponent = (props) => {
     let hideBefore = props.hideBefore ? "hideBefore" : ""
     let menuDescr = props.menuDescr ? "menuDescr" : ""
     let cardMenu = props.cardMenu ? "cardMenu" : ""
+    let bottomBorder = props.bottomBorder ? "bottomBorder" : ""
     return (
-        <div className={`card ${className} ${cardMenu}`}>
+        <div className={`card ${className} ${cardMenu} ${bottomBorder}`}>
             <picture className={`card__img ${shadow}`}>
                 <source media="(max-width: 680px)" srcSet={props.srcSet1} />
                 <source media="(max-width: 1080px)" srcSet={props.srcSet2} />
@@ -14,7 +15,7 @@ const CardComponent = (props) => {
                 <img src={props.src} srcSet={props.srcSet}
                     alt={props.alt} />
             </picture>
-            <div className={`card__main ${hideBefore}`}>
+            <div className={`card__main  ${hideBefore}`}>
                 <h2 className={`card__main__title ${menuTitle}`}>{props.title}</h2>
                 <p className={`card__main__descr ${menuDescr}`}>{props.description} </p>
             </div>
--- src/main/menu.js
+++ src/main/menu.js
@@ -35,10 +35,10 @@ const Menu = () => {
                     alt="Enjoy the delicious salmon"
                     title="Seared Salmon Fillet"
                     description=" Our locally sourced salmon served with
-                 a refreshing buckwheat summer salad."
-                    menuTitle={true} cardShadow={true} hideBefore={true} menuDescr={true} cardMenu={true} />
-
-                <hr />
+                    a refreshing buckwheat summer salad."
+                    menuTitle={true} cardShadow={true} hideBefore={true} menuDescr={true}
+                    bottomBorder={true} cardMenu={true} />
+                
 
                 <CardComponent srcSet1={`${beefMobile}, ${beefMobile2x}`}
                     srcSet2={`${beefTabletDesktop}`}
@@ -49,9 +49,10 @@ const Menu = () => {
                     title="Rosemary Filet Mignon"
                     description="Our prime beef served to your taste with a
                      delicious choice of seasonal sides."
-                    menuTitle={true} cardShadow={true} hideBefore={true} menuDescr={true} cardMenu={true} />
+                    menuTitle={true} cardShadow={true} hideBefore={true} menuDescr={true}
+                    bottomBorder={true} cardMenu={true} />
 
-                <hr />
+                
 
                 <CardComponent srcSet1={`${chocolateMobile}, ${chocolateMobile2x}`}
                     srcSet2={`${chocolateTabletDesktop}`}
@@ -62,7 +63,8 @@ const Menu = () => {
                     title="Summer Fruit Chocolate Mousse"
                     description="Creamy mousse combined with summer fruits
                      and dark chocolate shavings."
-                    menuTitle={true} cardShadow={true} hideBefore={true} menuDescr={true} cardMenu={true} />
+                    menuTitle={true} cardShadow={true} hideBefore={true} menuDescr={true}
+                     cardMenu={true} />
 
             </div>
         </section>
