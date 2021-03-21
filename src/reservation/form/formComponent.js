3f1189d1096f59d300660aeb1a56f5510f28739d
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-03-21 21:41:14 +0200

    feat: date and time selection styling

--- src/reservation/form/formComponent.js
+++ src/reservation/form/formComponent.js
@@ -18,70 +18,93 @@ const FormComponent = () => {
             </label>
 
             <div className="date__selection">
-                
-                <fieldset>
-                    <legend>MM</legend>
-                    <select name="month__zone">                        
-                        <option value="1">Jan</option>
-                        <option value="2">Feb</option>
-                        <option value="3">Mar</option>
-                        <option value="4">Apr</option>
-                        <option value="5">May</option>
-                        <option value="6">Jun</option>
-                        <option value="7">Jul</option>
-                        <option value="8">Aug</option>
-                        <option value="9">Sep</option>
-                        <option value="10">Oct</option>
-                        <option value="11">Nov</option>
-                        <option value="12">Dec</option>
-                    </select>
-                </fieldset>
+                <h3 className="date__selection__title">Pick a date</h3>
+                <div className="date__selection__list">
+                    <fieldset>
+                        <legend>MM</legend>
+                        <select name="month__zone">
+                            <option value="1">01</option>
+                            <option value="2">02</option>
+                            <option value="3">03</option>
+                            <option value="4">04</option>
+                            <option value="5">05</option>
+                            <option value="6">06</option>
+                            <option value="7">07</option>
+                            <option value="8">08</option>
+                            <option value="9">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                    </fieldset>
 
-                <fieldset>
-                    <legend>DD</legend>
-                    <select name="day__zone">                       
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
+                    <fieldset>
+                        <legend>DD</legend>
+                        <select name="day__zone">
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>YYYY</legend>
+                        <select name="year__zone">
+                            <option value="1">2021</option>
+                            <option value="2">2022</option>
+                            <option value="3">2023</option>
+                        </select>
+                    </fieldset>
+                </div>
+            </div>
+            <div className="time__selection">
+                <h3 className="time__selection__title">Pick a time</h3>
+                <div className="time__selection__list">
+                    <select className="hours">
+                        <option value="03">03</option>
+                        <option value="06">06</option>
+                        <option value="09">09</option>
                         <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
+                    </select>
+                    <select className="minutes">
+                        <option value="00">00</option>
                         <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
                         <option value="30">30</option>
-                        <option value="31">31</option>
+                        <option value="45">45</option>
                     </select>
-                </fieldset>
-
-                <fieldset>
-                    <legend>YYYY</legend>
-                    <select name="year__zone">
-                        <option value="1">2021</option>
-                        <option value="2">2022</option>
-                        <option value="3">2023</option>
+                    <select className="mode">
+                        <option value="am">am</option>
+                        <option value="pm">pm</option>                        
                     </select>
-                </fieldset>
+                </div>
             </div>
 
             <input type="submit" />
